878b380b47a0d68b61633f5a87fc4ce368b4d8bb
Nathan Kolpa <nathan@kolpa.me>
2023-10-01 20:20:36 +0200

    Create syscall crate

--- libraries/user/src/lib.rs
+++ libraries/user/src/lib.rs
@@ -1,4 +1,3 @@
 #![no_std]
 
 pub mod syscalls;
-pub mod ipc;
--- libraries/user/src/syscalls.rs
+++ libraries/user/src/syscalls.rs
@@ -8,4 +8,6 @@ pub use error::*;
 ///
 /// This function is unsafe because arguments can be interpreted as pointers.
 /// The caller must ensure that the rust borrow checker rule's are respected on order to guarantee safety.
-pub unsafe fn syscall(syscall: u64, arg0: u64, arg1: u64, arg2: u64, arg3: u64) {}
+pub unsafe fn syscall(syscall: u64, arg0: u64, arg1: u64, arg2: u64, arg3: u64) {
+
+}
